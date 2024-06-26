4fc137f6abba75e146c19a3f27cca3cd63f20d07
Fegor2002 <enwa@ualberta.ca>
2024-06-25 18:21:41 -0600

    website works to turn led on and off

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-async-web-server-espasyncwebserver-library/
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
 #include <Arduino.h>
 #include <iostream>
+
 #include <bits/stdc++.h> 
 #include <random>
 #include <chrono>
@@ -7,15 +15,18 @@
 #include <vector>
 #include <string>
 #include <string.h>
-#include "WiFi.h"
+
 #include <ESP32Servo.h>
 #include <Keypad.h>
 #include <SPI.h>
 #include "FS.h"
+
+#include "WiFi.h"
 #include <ESPAsyncWebServer.h>
+#include <AsyncTCP.h>
 
-AsyncWebServer server(80);
-
+#define LED 2
+//set WiFi networ ID and password
 #define WIFI_NETWORK "dlink-9728"
 #define WIFI_PASSWORD "kugxb48486"
 
@@ -29,6 +40,68 @@ AsyncWebServer server(80);
 #define pushButton 4
 #define RedLED 27
 #define A1 34
+
+//webserver object
+AsyncWebServer server(80);
+
+// define parameters to be passed to the URL (GPIO number and states)
+const char* PARAM_INPUT_1 = "output";
+const char* PARAM_INPUT_2 = "state";
+
+// webpage
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <title>ESP Web Server</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="data:,">
+  <style>
+    html {font-family: Arial; display: inline-block; text-align: center;}
+    h2 {font-size: 3.0rem;}
+    p {font-size: 3.0rem;}
+    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+    .switch input {display: none}
+    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
+    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
+    input:checked+.slider {background-color: #b30000}
+    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
+  </style>
+</head>
+<body>
+  <h2>ESP Web Server</h2>
+  %BUTTONPLACEHOLDER%
+<script>function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?output="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?output="+element.id+"&state=0", true); }
+  xhr.send();
+}
+</script>
+</body>
+</html>
+)rawliteral";
+
+String outputState(int output){
+  if(digitalRead(output)){
+    return "checked";
+  }
+  else {
+    return "";
+  }
+}
+
+// Replaces placeholder with button section in your web page
+String processor(const String& var){
+  //Serial.println(var);
+  if(var == "BUTTONPLACEHOLDER"){
+    String buttons = "";
+    buttons += "<h4>Output - GPIO 2</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"2\" " + outputState(LED) + "><span class=\"slider\"></span></label>";
+    return buttons;
+  }
+  return String();
+}
+
 
 int position = 0;
 int bias = 40;
@@ -105,14 +178,38 @@ void setup() {
   Serial.begin(9600); // Start the serial communication
   delay(1000);        // Wait for serial port to initialize (useful for some boards)
   connectToWiFi();    // Function to connect to WiFi
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
 
+
+  // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-  request->send(200, "text/html", "<h1> Whats Popping Dickhead! from ESP32 </h1>");
+    request->send_P(200, "text/html", index_html, processor);
   });
 
+  // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage1;
+    String inputMessage2;
+    // GET input1 value on <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
+    if (request->hasParam(PARAM_INPUT_1) && request->hasParam(PARAM_INPUT_2)) {
+      inputMessage1 = request->getParam(PARAM_INPUT_1)->value();
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      digitalWrite(inputMessage1.toInt(), inputMessage2.toInt());
+    }
+    else {
+      inputMessage1 = "No message sent";
+      inputMessage2 = "No message sent";
+    }
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Start server
   server.begin();
   Serial.println("HTTP server started");
 
+
+  //define hardware modes
   pinMode(pushButton, INPUT_PULLUP);
   pinMode(GreenLED, OUTPUT);
   pinMode(RedLED, OUTPUT);
